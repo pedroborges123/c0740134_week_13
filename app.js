c3d3b73dea0621c6a40b8059c582d8072da389a4
Pedro_Borges <pedroborges123@gmail.com>
2019-04-04 10:47:30 -0400

    adding another file

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var http = require('http');
 var url = require('url');
 
 http.createServer(function (req, res) {
+
     res.writeHead(200, {'Content-Type': 'text/html'});
     if(req.url == "/test"){
         res.write("Teste... ");
---
+++ looping.js
@@ -0,0 +1,5 @@
+let user = {first_name: "CSS", last_name: "HTML", age: 4, website: "java2s.com"};
+
+for(key in user){
+    console.log(key);
+}
