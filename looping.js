4ae5f1f4cd9db3da14c239fd42737866b4a23dfe
Pedro_Borges <pedroborges123@gmail.com>
2019-04-04 10:59:04 -0400

    sum 1 to 11

--- looping.js
+++ looping.js
@@ -3,3 +3,12 @@ let user = {first_name: "CSS", last_name: "HTML", age: 4, website: "java2s.com"}
 for(key in user){
     console.log(key);
 }
+
+let sum = 0;
+let i = 1;
+while(i <12){
+    console.log(i);
+    sum += i;
+    i++;
+}
+console.log("The sum is "+ sum);
